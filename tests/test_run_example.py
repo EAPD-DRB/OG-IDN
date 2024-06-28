741f41b5156deeaecc1fcd1d19b59bb3713b5149
jdebacker <jason.debacker@gmail.com>
2024-06-27 23:05:40 -0400

    replace zaf instances

--- tests/test_run_example.py
+++ tests/test_run_example.py
@@ -1,5 +1,5 @@
 """
-This model tests whether using the `OG-IDN/examples/run_og_zaf.py`
+This model tests whether using the `OG-IDN/examples/run_og_idn.py`
 work by making sure that it does not break (is still running) after
 5 minutes (300 seconds).
 """
@@ -13,23 +13,23 @@ from pathlib import Path
 import pytest
 
 
-def call_run_og_zaf():
+def call_run_og_idn():
     cur_path = os.path.split(os.path.abspath(__file__))[0]
     path = Path(cur_path)
     roe_fldr = os.path.join(path, "..", "examples")
-    roe_file_path = os.path.join(roe_fldr, "run_og_zaf.py")
+    roe_file_path = os.path.join(roe_fldr, "run_og_idn.py")
     spec = importlib.util.spec_from_file_location(
-        "run_og_zaf.py", roe_file_path
+        "run_og_idn.py", roe_file_path
     )
     roe_module = importlib.util.module_from_spec(spec)
-    sys.modules["run_og_zaf.py"] = roe_module
+    sys.modules["run_og_idn.py"] = roe_module
     spec.loader.exec_module(roe_module)
     roe_module.main()
 
 
 @pytest.mark.local
-def test_run_og_zaf(f=call_run_og_zaf):
-    p = multiprocessing.Process(target=f, name="run_og_zaf", args=())
+def test_run_og_idn(f=call_run_og_idn):
+    p = multiprocessing.Process(target=f, name="run_og_idn", args=())
     p.start()
     time.sleep(300)
     if p.is_alive():
@@ -37,7 +37,7 @@ def test_run_og_zaf(f=call_run_og_zaf):
         p.join()
         timetest = True
     else:
-        print("run_og_zaf.py did not run for minimum time")
+        print("run_og_idn.py did not run for minimum time")
         timetest = False
     print("timetest ==", timetest)
 
